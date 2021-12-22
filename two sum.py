be9f0b7f250b241d35380b561635f2d8cef71a11
GarrettRector <garrett@grector.dev>
2021-12-21 20:29:58 -0500

    two sum

--- two sum.py
+++ two sum.py
@@ -1,6 +1,11 @@
-class Solution(object):
-    def twoSum(self, nums, target):
+class Solution:
+    def twoSum(self, nums: list, target: int) -> list[int, int]:
         for i in range(len(nums)):
             for n in range(i+1, len(nums)):
                 if nums[i] + nums[n] == target:
                     return [i, n]
+
+
+if __name__ == "__main__":
+    s = Solution()
+    s.twoSum([3, 2, 4], 6)
