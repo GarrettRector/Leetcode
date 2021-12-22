07f540e7149e120d679d70cc71f2e3f29bcba19d
GarrettRector <garrett@grector.dev>
2021-12-21 20:29:31 -0500

    palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,21 +1,26 @@
-class Solution(object):
-    def isPalindrome(self, x):
+class Solution:
+    def isPalindrome(self, x: str):
         y = False
-        
+
         x = str(x)
         if x[0] == "-":
             y = True
             x = x[1:]
-        
+
         solution = ""
-        
+
         for i in range(len(str(x))):
-            solution += x[len(x)-i-1]
-        
+            solution += x[len(x) - i - 1]
+
         if y:
             solution = "-" + solution
-        
+
         if solution == x:
             return True
         else:
             return False
+
+
+if __name__ == "__main__":
+    s = Solution()
+    s.isPalindrome("racecar")
