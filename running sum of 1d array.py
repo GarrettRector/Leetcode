28ceb4c506f2082296c66951298bf37a75a08ae2
Garrett Rector <garrett@grector.dev>
2022-02-10 12:09:10 -0500

    Update running sum of 1d array.py

--- running sum of 1d array.py
+++ running sum of 1d array.py
@@ -1,6 +1,11 @@
-class Solution(object):
+class Solution:
     def runningSum(self, nums):
         ans = []
         for i, num in enumerate(nums):
             ans.append(sum(nums[:i]) + num)
         return ans
+    
+    
+if __name__ == "__main__":
+    s = Solution()
+    s.runningSum([1, 2, 3, 4])
