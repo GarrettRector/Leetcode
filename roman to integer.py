54ab98d72692de67f7c4a8c3c2bb7773479ffbe1
garrettrector <garrett@grector.dev>
2022-02-10 12:18:32 -0500

    added a +=

--- roman to integer.py
+++ roman to integer.py
@@ -5,7 +5,7 @@ class Solution:
         rul = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         for ch in s:
             ch_val = int(rul[ch])
-            r_sum = r_sum + ch_val
+            r_sum += ch_val
             if temp < ch_val:
                 r_sum -= temp * 2
             temp = ch_val
