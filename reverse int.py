22fa3799e16a3f015cff9bf71649b14e6a741dc8
garrettrector <garrett@grector.dev>
2022-02-10 12:18:10 -0500

    changed for loop to a join

--- reverse int.py
+++ reverse int.py
@@ -2,26 +2,20 @@ class Solution:
     def reverse(self, x: int) -> int:
         
         y = False
-        
+
         x = str(x)
         if x[0] == "-":
             y = True
             x = x[1:]
-        
-        solution = ""
-        
-        for i in range(len(str(x))):
-            solution += x[len(x)-i-1]
-        
+
+        solution = "".join(x[len(x)-i-1] for i in range(len(str(x))))
+
         if y:
             solution = "-" + solution
-            
+
         solution = int(solution)
-        if solution > 2147483650:
+        if solution > 2147483650 or solution < -2147483650:
             solution = 0
-        elif solution < -2147483650:
-            solution = 0
-        
         return solution
 
 
