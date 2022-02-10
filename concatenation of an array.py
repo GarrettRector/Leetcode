ada7ac3f69eac711c8bf3e688cc3481b8f5360a1
garrettrector <garrett@grector.dev>
2022-02-10 12:17:34 -0500

    added if name clause

--- concatenation of an array.py
+++ concatenation of an array.py
@@ -1,3 +1,8 @@
-class Solution(object):
+class Solution:
     def getConcatenation(self, nums):
         return nums + nums
+
+
+if __name__ == "__main__":
+    s = Solution()
+    s.getConcatenation([1, 2, 3])
--- keyboard row.py
+++ keyboard row.py
@@ -1,12 +1,13 @@
 class Solution:
-    def findWords(self, words: List[str]) -> List[str]:
+    def findWords(self, words):
         firstRow, secondRow, thirdRow = set('qwertyuiop'), set('asdfghjkl'), set('zxcvbnm')
         ans = []
         for each in words:
             letters = set(each.lower())
-            if len(letters-firstRow)==0 or len(letters-secondRow)==0 or len(letters-thirdRow)==0:
+            if len(letters - firstRow) == 0 or len(letters - secondRow) == 0 or len(letters - thirdRow) == 0:
                 ans.append(each)
         return ans
+
 
 if __name__ == "__main__":
     s = Solution()
