792f6f85b01aaa5982522e36d16e7b86baeb27e6
garrettrector <garrett@grector.dev>
2022-02-10 14:57:56 -0500

    perfect number

---
+++ perfect number.py
@@ -0,0 +1,12 @@
+import math
+
+
+class Solution:
+    def checkPerfectNumber(self, num):
+        s = 1 + sum(i + num // i for i in range(2, int(math.sqrt(num)) + 1) if num % i == 0)
+        return num == s if num > 1 else False
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.checkPerfectNumber(30402457))
