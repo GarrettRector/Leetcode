d4ce81e2e9507cdcf6c97befc3df973ae9ff2491
garrettrector <garrett@grector.dev>
2022-02-10 12:16:28 -0500

    sum_ may not have been defined

--- add digits.py
+++ add digits.py
@@ -1,13 +1,17 @@
-class Solution(object):
+class Solution:
     def addDigits(self, number):
         number = str(number)
         done = False
+        sum_ = None
         while not done:
             n = [int(num) for num in number]
-            print(n)
             sum_ = sum(n)
             number = str(sum_)
             if sum_ < 10:
                 done = True
         return sum_
-        
+
+
+if __name__ == "__main__":
+    s = Solution()
+    s.addDigits(38)
