7cde1a3ad5629787ee7b8f3ae4931c5d5c312f55
garrettrector <garrett@grector.dev>
2022-02-15 12:02:39 -0500

    single number

---
+++ single number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums):
+        for num in set(nums):
+            if nums.count(num) == 1:
+                return num
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.singleNumber([4, 1, 2, 1, 2]))
