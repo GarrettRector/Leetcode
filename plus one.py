f03f9c585c7db9a23fab4b237cc0d3495bb70703
garrettrector <garrett@grector.dev>
2022-02-22 12:05:06 -0500

    plus one

---
+++ plus one.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def plusOne(self, digits):
+        for i, digit in enumerate(digits):
+            digits[i] = str(digit)
+        return [int(i) for i in str(int("".join(digits)) + 1)]
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.plusOne([4,3,2,1]))
